c3d3434da52bb110b45090bb3e1c8717670f43b5
Soibi Sharon Ephraim-George <128920191+Soibi699@users.noreply.github.com>
2023-11-14 01:18:56 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module that prints details of an object"""
-
-
-def lookup(obj):
-    """function that prints the details"""
-    return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module that compares an object to a class"""
-
-
-def is_same_class(obj, a_class):
-    """function that compares"""
-    return (type(obj) == a_class)
